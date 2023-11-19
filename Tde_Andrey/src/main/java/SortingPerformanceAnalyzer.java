c08ea25c33b50a2e16a479d940667f1f2d57b8a7
JVZambao <joao.zambao@pucpr.edu.br>
2023-11-19 17:13:44 -0300

    Penultima Verção

--- Tde_Andrey/src/main/java/SortingPerformanceAnalyzer.java
+++ Tde_Andrey/src/main/java/SortingPerformanceAnalyzer.java
@@ -6,31 +6,38 @@ import TDE_Sort.BubbleSort;
 import TDE_Sort.MergeSort;
 import TDE_Sort.QuickSort;
 
-
 public class SortingPerformanceAnalyzer {
     public static void main(String[] args) {
-        //Trocar o tamanho do vetor entre (50, 500, 1000, 5000 e 10000)
-        int[] randomNumbers = generateRandomArray(10000, 1, 999);
+        // Trocar o tamanho do vetor entre (50, 500, 1000, 5000 e 10000)
+        int[] randomNumbers = generateRandomArray(50, 1, 999);
         QuickSort quickSort = new QuickSort();
         MergeSort mergeSort = new MergeSort();
         BubbleSort bubbleSort = new BubbleSort();
 
         // Copia a mesma lista desordenada para as três classes
-        int[] randomNumbersCopy = Arrays.copyOf(randomNumbers, randomNumbers.length);
+        int[] randomNumbersCopy = Arrays.copyOf(randomNumbers, tamanho(randomNumbers));
         quickSort.sort(randomNumbersCopy);
-        mergeSort.sort(Arrays.copyOf(randomNumbers, randomNumbers.length));
-        bubbleSort.sort(Arrays.copyOf(randomNumbers, randomNumbers.length));
+        mergeSort.sort(Arrays.copyOf(randomNumbers, tamanho(randomNumbers)));
+        bubbleSort.sort(Arrays.copyOf(randomNumbers, tamanho(randomNumbers)));
 
         try {
             FileWriter csvWriter = new FileWriter("sorting_performance.csv");
             csvWriter.append("Algorithm,Time (ns),Comparisons,Swaps,Original List,Sorted List\n");
-            writePerformanceData(csvWriter, "QuickSort", quickSort);
-            writePerformanceData(csvWriter, "MergeSort", mergeSort);
-            writePerformanceData(csvWriter, "BubbleSort", bubbleSort);
+            writePerformanceData(csvWriter, "QuickSort", quickSort, tamanho(randomNumbers));
+            writePerformanceData(csvWriter, "MergeSort", mergeSort, tamanho(randomNumbers));
+            writePerformanceData(csvWriter, "BubbleSort", bubbleSort, tamanho(randomNumbers));
             csvWriter.close();
             System.out.println("Performance data written to sorting_performance.csv");
         } catch (IOException e) {
         }
+    }
+
+    private static int tamanho(int[] array) {
+        int count = 0;
+        for (int value : array) {
+            count++;
+        }
+        return count;
     }
 
     private static int[] generateRandomArray(int size, int minValue, int maxValue) {
@@ -42,7 +49,7 @@ public class SortingPerformanceAnalyzer {
         return array;
     }
 
-    private static void writePerformanceData(FileWriter writer, String algorithm, BubbleSort sortingAlgorithm) throws IOException {
+    private static void writePerformanceData(FileWriter writer, String algorithm, BubbleSort sortingAlgorithm, int arrayLength) throws IOException {
         writer.append(algorithm + ",");
         writer.append(Long.toString(sortingAlgorithm.getExecutionTime()) + ",");
         writer.append(Integer.toString(sortingAlgorithm.getComparisons()) + ",");
@@ -50,7 +57,8 @@ public class SortingPerformanceAnalyzer {
         writer.append(Arrays.toString(sortingAlgorithm.getOriginalArray()) + ",");
         writer.append(Arrays.toString(sortingAlgorithm.getSortedArray()) + "\n");
     }
-    private static void writePerformanceData(FileWriter writer, String algorithm, MergeSort sortingAlgorithm) throws IOException {
+
+    private static void writePerformanceData(FileWriter writer, String algorithm, MergeSort sortingAlgorithm, int arrayLength) throws IOException {
         writer.append(algorithm + ",");
         writer.append(Long.toString(sortingAlgorithm.getExecutionTime()) + ",");
         writer.append(Integer.toString(sortingAlgorithm.getComparisons()) + ",");
@@ -58,7 +66,8 @@ public class SortingPerformanceAnalyzer {
         writer.append(Arrays.toString(sortingAlgorithm.getOriginalArray()) + ",");
         writer.append(Arrays.toString(sortingAlgorithm.getSortedArray()) + "\n");
     }
-    private static void writePerformanceData(FileWriter writer, String algorithm, QuickSort sortingAlgorithm) throws IOException {
+
+    private static void writePerformanceData(FileWriter writer, String algorithm, QuickSort sortingAlgorithm, int arrayLength) throws IOException {
         writer.append(algorithm + ",");
         writer.append(Long.toString(sortingAlgorithm.getExecutionTime()) + ",");
         writer.append(Integer.toString(sortingAlgorithm.getComparisons()) + ",");
@@ -66,5 +75,4 @@ public class SortingPerformanceAnalyzer {
         writer.append(Arrays.toString(sortingAlgorithm.getOriginalArray()) + ",");
         writer.append(Arrays.toString(sortingAlgorithm.getSortedArray()) + "\n");
     }
-    
 }
